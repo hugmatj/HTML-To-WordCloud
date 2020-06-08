ee474623fd7be25f3d9df709b9dfa336873c5aab
Aaron Paul Henry <aaron51598@gmail.com>
2020-06-08 18:42:12 -0400

    Added comments in code

--- HTMLtoWordCloud.py
+++ HTMLtoWordCloud.py
@@ -9,11 +9,10 @@ Provided by Gutenberg Project
 """
 
 import nltk
-from nltk.stem import PorterStemmer
 import numpy as np
 from PIL import Image
 
-
+""" place your URL or file:c/// here """
 import urllib.request
 response =  urllib.request.urlopen('https://www.gutenberg.org/files/2554/2554-h/2554-h.htm')
 html = response.read()
@@ -28,6 +27,7 @@ text = soup.get_text(strip = True)
 tokens = [t for t in text.split()]
 print(tokens)
 
+""" add any words you wish to delete from the scraped content here using sr.append() """
 words = set(nltk.corpus.words.words())
 from nltk.corpus import stopwords
 sr= stopwords.words('english')
@@ -41,17 +41,12 @@ for token in tokens:
         
 reduced_tokens = list(word.lower() for word in clean_tokens if word.isalpha() and len(word) > 3 and len(word) < 15)
 
-
-ps = PorterStemmer()
-stemmed_words=[]
-for w in reduced_tokens:
-    stemmed_words.append(ps.stem(w))
-
-freq = nltk.FreqDist(stemmed_words)
+""" makes a nice plot for you to visualize what words are most common """
+freq = nltk.FreqDist(reduced_tokens)
 
 freq.plot(30, cumulative=False)
 
-
+""" shape image for the wordcloud mask """
 char_mask = np.array(Image.open("C:/Users/aaron/.spyder-py3/axe.jpg"))    
 ##image_colors = ImageColorGenerator(char_mask)
 
